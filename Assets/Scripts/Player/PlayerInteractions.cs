aca8f5f84caf46d59766664717e10ed4e104cc83
Ville Heikkinen <heivil88@gmail.com>
2019-09-29 18:32:02 +0300

    Started with player interactions, but the input system is confusion :D

---
+++ Assets/Scripts/Player/PlayerInteractions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    [SerializeField]
+    private string _useButton = "Use";
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.GetComponentInParent<IUseable>() != null)
+        {
+            //probably not like this. Don't know how our input system works :)
+            if(Input.GetButtonDown(_useButton))
+            collision.GetComponentInParent<PlayerUseable>().Use(gameObject);
+        }
+    }
+}
