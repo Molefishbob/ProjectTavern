d73cc6e2e13074ea41a0cd5d5a0a1dae1f66eb07
Ville Heikkinen <heivil88@gmail.com>
2019-10-30 13:34:01 +0200

    Very basic menu functionality

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject _settingsMenu;
+    public GameObject _quitMenu;
+
+
+    public void OpenSettingsMenu()
+    {
+        _settingsMenu.SetActive(true);
+        _quitMenu.SetActive(false);
+    }
+
+    public void OpenQuitMenu()
+    {
+        _settingsMenu.SetActive(false);
+        _quitMenu.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
