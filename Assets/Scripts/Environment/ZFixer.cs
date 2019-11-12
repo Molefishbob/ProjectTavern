b76213988e792ba1d0fe6fa3ef0f337b5d7889cd
VoidCooper <santeri.sivula@tuni.fi>
2019-11-12 10:54:06 +0200

    Zfixer and added it to stuff

---
+++ Assets/Scripts/Environment/ZFixer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZFixer : MonoBehaviour
+{
+    [SerializeField, Tooltip("How many frames between changes")] private int _refreshRate = 5;
+    private int _counter = 0;
+
+#pragma warning disable CS0649
+    [SerializeField] private SpriteRenderer _renderer;
+#pragma warning restore CS0649
+
+    private void Awake()
+    {
+        if (_renderer == null)
+            _renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        _counter++;
+        if (_counter <= _refreshRate)
+        {
+            _counter = 0;
+            _renderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 10);
+        }
+    }
+}
