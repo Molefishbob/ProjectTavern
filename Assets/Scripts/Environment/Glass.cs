6dbe482d287f0267d4ab32dd14222f91d4445820
Ville Heikkinen <heivil88@gmail.com>
2019-11-12 13:41:15 +0200

    Glass works

--- Assets/Scripts/Environment/Glass.cs
+++ Assets/Scripts/Environment/Glass.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Managers.BeverageManager;
 
 public class Glass : MonoBehaviour
 {
-    public List<Ingredient.DrinkIngredient> _ingredients = new List<Ingredient.DrinkIngredient>();
+    public List<Ingredient.DrinkIngredient> _currentIngredients = new List<Ingredient.DrinkIngredient>();
     private List<Drink> _drinks;
     private Drink[] _possibleDrinks = new Drink[10];
     private Drink _currentDrink;
@@ -22,14 +21,9 @@ public class Glass : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        CheckCurrentDrink();
-    }
-
     public void AddIngredient(Ingredient.DrinkIngredient ingredient)
     {
-        _ingredients.Add(ingredient);
+        _currentIngredients.Add(ingredient);
 
         CheckCurrentDrink();
     }
@@ -38,32 +32,51 @@ public class Glass : MonoBehaviour
     {
         for (int i = 0; i < _possibleDrinks.Length; i++)
         {
-            if (_possibleDrinks[i]._ingredients.Count > _ingredients.Count)
+            if (_possibleDrinks[i] == null) continue;
+
+            if (_possibleDrinks[i]._ingredients.Count > _currentIngredients.Count)
             {
-                for (int a = 0; a < _possibleDrinks[i]._ingredients.Count; a++)
-                {
-                    _possibleDrinks[i] = null;
-                }
+                _possibleDrinks[i] = null;
             }
             else
             {
-                for (int b = 0; b < _ingredients.Count; b++)
+                bool doesContain = false;
+                for (int b = 0; b < _currentIngredients.Count; b++)
                 {
-                    if (!_possibleDrinks[i]._ingredients.Contains(_ingredients[b]))
+                    if (_possibleDrinks[i]._ingredients.Contains(_currentIngredients[b]))
                     {
-                        _possibleDrinks[i] = null;
-                        _currentDrink = null;
+                        doesContain = true;
                     }
                     else
                     {
-                        _currentDrink = _possibleDrinks[i];
+                        doesContain = false;
+                        break;
                     }
                 }
+                if (doesContain)
+                {
+                    _currentDrink = _possibleDrinks[i];
+                }
             }
-            
-        }
 
-        print(_currentDrink);
+        }
+        if(_currentDrink = null)
+        {
+            Debug.Log("Warning, no valid drink!");
+        }
+        
+    }
+
+    public void EmptyGlass()
+    {
+        int i = 0;
+        foreach (Drink drink in _drinks)
+        {
+            _possibleDrinks[i] = drink;
+            i++;
+        }
+        _currentIngredients.Clear();
+        _currentDrink = null;
     }
 
 }
