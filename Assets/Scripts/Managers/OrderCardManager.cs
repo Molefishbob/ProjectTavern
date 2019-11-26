9405ca1d85cf05703424cb34a67833c2eb1493be
VoidCooper <santeri.sivula@tuni.fi>
2019-11-26 14:46:58 +0200

    Add cards

--- Assets/Scripts/Managers/OrderCardManager.cs
+++ Assets/Scripts/Managers/OrderCardManager.cs
@@ -2,24 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrderCardManager : MonoBehaviour
+namespace Managers
 {
-    public static OrderCardManager Instance { get; private set; }
-    public bool Initialized { get; private set; }
-    public static bool DoesExist { get => Instance != null; }
-
-    private void Awake()
+    public class OrderCardManager : MonoBehaviour
     {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(gameObject);
-    }
+        public static OrderCardManager Instance { get; private set; }
+        public bool Initialized { get; private set; }
+        public static bool DoesExist { get => Instance != null; }
 
-    private void OnDestroy()
-    {
-        if (Instance == this)
-            Instance = null;
-    }
+        private OrderCard _cardTemplate = null;
+        private List<OrderCard> _spawnedCards = new List<OrderCard>();
 
+        public bool test = false;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+
+            _cardTemplate = transform.GetChild(0).GetComponent<OrderCard>();
+            _cardTemplate.gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+
+        public void AddCard(Customer.MyOrder order, Customer from = null)
+        {
+            foreach (var item in _spawnedCards)
+                if (item.Order._drinkOrder == order._drinkOrder || order._order != PlayerState.Holdables.Drink)
+                    return;
+
+            _spawnedCards.Add(Instantiate(_cardTemplate, transform));
+            _spawnedCards[_spawnedCards.Count - 1].SetInfo(order, from);
+            _spawnedCards[_spawnedCards.Count - 1].gameObject.SetActive(true);
+
+            for (int i = 0; i < _spawnedCards.Count - 1; i++)
+            {
+                _spawnedCards[i].Move(new Vector2(-2, 0), true);
+            }
+        }
+
+        public void RemoveCard(Customer.MyOrder order, Customer from = null)
+        {
+            throw new System.NotImplementedException("Kikkeli 8==D");
+        }
+
+        private void Update()
+        {
+            if (test)
+            {
+                AddCard(new Customer.MyOrder(PlayerState.Holdables.Drink, (Managers.BeverageManager.Beverage) Random.Range(0, 11)));
+                test = false;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Menus/OrderCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderCard : MonoBehaviour
+{
+    public Customer.MyOrder Order { get; private set; }
+    public Customer FromCustomer { get; private set; }
+    [HideInInspector]
+    public RectTransform RectTransform { get => (RectTransform)transform; }
+
+    public void SetInfo(Customer.MyOrder order, Customer from = null)
+    {
+        Order = order;
+        FromCustomer = from;
+    }
+
+    public void Move(Vector2 position, bool relative)
+    {
+        if (relative)
+        {
+            transform.position += new Vector3(position.x, position.y);
+        }
+        else
+        {
+            transform.position = new Vector3(position.x, position.y);
+        }
+    }
+}
